6defbcc70c4a715839eb7e97b21ac69a4d8196bd
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-22 21:41:52 +0200

    feat: app background colour

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1> jsMLA </h1>
+    <NavigationBar/>
 </template>
 
 <script>
+import NavigationBar from '@/components/NavigationBar/NavigationBar'
 
 export default {
   name: 'App',
   components: {
-
+    NavigationBar
   }
 }
 </script>
