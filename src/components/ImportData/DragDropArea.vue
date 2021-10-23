f9d8ab8f8bcaa1e015f75794a7205c0f43855eb9
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-23 19:04:05 +0200

    #19 feat: drag and drop area

---
+++ src/components/ImportData/DragDropArea.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col border-black border-2 rounded-3xl border-dashed p-10 m-20 w-max">
+        <BrowseFilesButton class="w-max"/>
+    </div>
+</template>
+
+<script>
+import BrowseFilesButton from "@/components/ImportData/BrowseFilesButton";
+
+export default {
+    name: "DragDropArea",
+    components: {
+        BrowseFilesButton
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
