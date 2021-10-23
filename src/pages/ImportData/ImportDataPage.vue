8566ebd1fa0c93c1b3256c4a66d7d02d94c2707b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-23 18:23:55 +0200

    #17 feat: import data page layout

---
+++ src/pages/ImportData/ImportDataPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <section id="import_data_area" class="h-full w-full flex justify-center content-center">
+        <div class="bg-white rounded-3xl w-full m-24">
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ImportDataPage.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
