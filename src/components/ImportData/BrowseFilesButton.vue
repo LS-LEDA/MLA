5d7924038f35e75299943049f98222d2fa4b41af
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-23 19:03:35 +0200

    #19 feat: browse file button

---
+++ src/components/ImportData/BrowseFilesButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button type="button" class="flex flex-row bg-blue-400 hover:bg-blue-500 rounded-lg px-4 py-2 mx-2 my-2 font-bold">
+        <svg-icon
+            type="mdi" :path="open_folder_icon"></svg-icon>
+        <span class="ml-3">Browse Files</span>
+    </button>
+</template>
+
+<script>
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiFolderOpen } from '@mdi/js'
+
+export default {
+    name: "BrowseFilesButton",
+    components: {
+        SvgIcon
+    },
+    data() {
+        return {
+            open_folder_icon: mdiFolderOpen
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
