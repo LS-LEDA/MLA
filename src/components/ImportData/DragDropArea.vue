6f9fda916bc8b607a2e22871442d2db6f6f33e1f
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-23 19:14:23 +0200

    #19 feat: input file type

--- src/components/ImportData/DragDropArea.vue
+++ src/components/ImportData/DragDropArea.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="flex flex-col border-black border-2 rounded-3xl border-dashed p-10 m-20 w-max">
+        <input type="file" ref="moodle_file" class="hidden">
         <BrowseFilesButton class="w-max"/>
     </div>
 </template>
