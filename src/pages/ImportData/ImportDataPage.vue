1959d47ebb03fa140e8ad734d8abda4c27ce0508
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-28 10:52:49 +0200

    #24 feat: add upload confirmation in import data page - default: hidden

--- src/pages/ImportData/ImportDataPage.vue
+++ src/pages/ImportData/ImportDataPage.vue
@@ -3,15 +3,23 @@
         <div class="flex bg-white rounded-3xl w-full m-24 content-center justify-center">
             <DragDropArea/>
         </div>
+        <UploadConfirmation v-if="file_selected"/>
     </section>
 </template>
 
 <script>
 import DragDropArea from "@/components/ImportData/DragDropArea";
+import UploadConfirmation from "@/components/ImportData/UploadConfirmation";
 
 export default {
     name: "ImportDataPage",
+    data() {
+        return {
+            file_selected: false
+        }
+    },
     components: {
+        UploadConfirmation,
         DragDropArea
     }
 }
