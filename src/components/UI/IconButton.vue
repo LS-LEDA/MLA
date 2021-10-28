d0e8fea87b440bbc6d927ca2b51f69d71284c287
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-28 10:49:58 +0200

    feat: Button with icon component

---
+++ src/components/UI/IconButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button type="button" class="flex flex-row bg-blue-400 hover:bg-blue-500 rounded-lg px-4 py-2 mx-2 my-2 font-bold">
+        <svg-icon type="mdi" :path="icon"></svg-icon>
+        <span class="ml-3"> {{ type }} </span>
+    </button>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+
+export default {
+    name: "IconButton",
+    props: ['icon', 'type'],
+    components: {
+        SvgIcon
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
