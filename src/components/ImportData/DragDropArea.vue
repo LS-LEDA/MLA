a6e5b146c5f6484b7b4c0c07cfb2fd01a696969e
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-28 19:10:50 +0200

    #18 fix: right box overflow

--- src/components/ImportData/DragDropArea.vue
+++ src/components/ImportData/DragDropArea.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col border-black border-2 rounded-3xl border-dashed p-10 m-20 w-full justify-center items-center
+    <div class="flex flex-col border-black border-2 rounded-3xl border-dashed p-10 m-20 justify-center items-center
                 space-y-6"
                 @dragenter.prevent="toggleActive"
                 @dragleave.prevent="toggleActive"
