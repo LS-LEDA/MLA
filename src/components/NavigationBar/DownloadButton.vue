1d60f47c6f587f4f10fe800dea392a3de17bd330
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-29 13:29:15 +0200

    #11 feat: download button state and styles

--- src/components/NavigationBar/DownloadButton.vue
+++ src/components/NavigationBar/DownloadButton.vue
@@ -1,10 +1,26 @@
 <template>
-    <button type="button" class=" bg-blue-400 rounded-lg py-2 mx-2 font-bold uppercase align-bottom text-xl">Download</button>
+    <button type="button" class="flex bg-blue-400 rounded-lg px-4 py-2 mx-2 my-2
+            block font-bold uppercase justify-center">
+        <svg-icon type="mdi" :path="download_icon" v-if="!nav_state"></svg-icon>
+        <span v-if="nav_state">Download</span>
+    </button>
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiCloudDownload } from "@mdi/js";
+
 export default {
-  name: "DownloadButton"
+    name: "DownloadButton",
+    components: {
+        SvgIcon
+    },
+    props: ['nav_state'],
+    data() {
+        return {
+            download_icon: mdiCloudDownload,
+        }
+    }
 }
 </script>
 
