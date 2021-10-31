c7f8d189ee56a9092d3bf228e92044473cd0e7c3
Aleix <aleix.olle@students.salle.url.edu>
2021-10-31 12:38:10 +0100

    #21 feat: Creation of the information PopUp

---
+++ src/components/ImportData/InformationPopUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=" filter block row-start-1 row-end-1 bg-opacity-50 bg-black blur-xl w-screen h-screen">
+  </div>
+
+</template>
+<script>
+
+
+export default {
+  name: "InformationPopUp",
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
