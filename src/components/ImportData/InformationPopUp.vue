099c027f8f19724d21e5fd59fffab0e768b4ad30
Aleix <aleix.olle@students.salle.url.edu>
2021-11-01 17:58:28 +0100

    #21 feat: Information PopUp shows the video and a button to go back to Import area

--- src/components/ImportData/InformationPopUp.vue
+++ src/components/ImportData/InformationPopUp.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class=" grid grid-cols-1 grid-rows-1 absolute z-10 row-start-1 row-end-1 col-start-1 col-end-1 rounded-xl bg-blue-200 opacity-100 w-2/3 h-2/3 self-center justify-self-center " @click="hidePopUp">
-    <div style="position: relative; padding-bottom: 65.20618556701031%; left: 15%; top: 7%" >
-      <iframe class="absolute inset-0 w-2/3 h-2/3 self-center"  src="https://www.loom.com/embed/ed2bc470d04c4d02b9825e9954069b41" allowfullscreen >
+  <div class=" flex flex-col absolute z-10 row-start-1 row-end-1 col-start-1 col-end-1 rounded-xl bg-blue-200 opacity-100 w-1/2 h-2/3 self-center justify-self-center">
+    <div class ="relative w-4/5 h-4/5 self-center p-10" >
+      <iframe class="relative inset-0 w-full h-full"  src="https://www.loom.com/embed/ed2bc470d04c4d02b9825e9954069b41" allowfullscreen >
       </iframe>
     </div>
+    <button class="relative self-center bg-blue-400 hover:bg-blue-500 rounded-lg font-bold py-3 w-1/6 h-1/10" @click="hidePopUp">EXIT</button>
   </div>
 
 </template>
