fef88fccb57fffaa29625b17129c1ffb05acd2b9
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-02 12:09:22 +0100

    #32 fix: info & upload confirmation popup background blur

--- src/components/ImportData/InformationPopUp.vue
+++ src/components/ImportData/InformationPopUp.vue
@@ -1,11 +1,15 @@
 <template>
-      <div class=" flex flex-col absolute z-10 row-start-1 row-end-1 col-start-1 col-end-1 rounded-xl bg-blue-200 opacity-100 w-1/2 h-2/3 self-center justify-self-center">
-            <div class ="relative w-4/5 h-4/5 self-center p-10">
+    <div class="ml-2 flex flex-col absolute inset-0 justify-center place-items-center bg-gray-400 bg-opacity-75 transition-opacity"
+         role="dialog" aria-modal="true">
+        <div class="flex flex-col bg-blue-200 w-3/6 h-4/6 rounded-3xl z-50">
+            <div class ="w-4/5 h-4/5 self-center p-10">
                 <iframe class="relative inset-0 w-full h-full rounded-2xl"  src="https://www.loom.com/embed/ed2bc470d04c4d02b9825e9954069b41" allowfullscreen >
                 </iframe>
             </div>
-          <button class="relative self-center bg-blue-400 hover:bg-blue-500 rounded-lg font-bold py-3 w-1/6 h-1/10" @click="hidePopUp">EXIT</button>
-      </div>
+            <button class="self-center bg-blue-400 hover:bg-blue-500 rounded-lg font-bold py-3 w-1/6 h-1/10" @click="hidePopUp">EXIT</button>
+        </div>
+        <div class="absolute w-full h-full filter backdrop-blur-sm z-10"></div>
+    </div>
 </template>
 
 
