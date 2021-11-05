e5030a25bfff40a739c92e29b10a2169a555382c
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-05 15:28:36 +0100

    #41 feat: summary page layout

---
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="text-4xl font-extrabold"> Summary of all course interactions </h1>
+    <span class=" font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
+</template>
+
+<script>
+export default {
+    name: "Summary"
+}
+</script>
+
+<style scoped>
+
+</style>
