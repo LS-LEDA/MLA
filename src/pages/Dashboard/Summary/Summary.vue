9f318a081dce34155b95a9b558b1dc64dbd7bb34
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-05 18:42:04 +0100

    #39 feat: add interaction card in summary

--- src/pages/Dashboard/Summary/Summary.vue
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -1,11 +1,17 @@
 <template>
     <h1 class="text-4xl font-extrabold"> Summary of all course interactions </h1>
-    <span class=" font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
+    <span class="font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
+    <InteractionCard/>
 </template>
 
 <script>
+import InteractionCard from "@/components/Summary/InteractionCard";
+
 export default {
-    name: "Summary"
+    name: "Summary",
+    components: {
+        InteractionCard
+    }
 }
 </script>
 
