4476fbf42a0de07fd7b42fd8da464d765c47c1d1
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-05 15:22:20 +0100

    #35 feat: Dashboard page route

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ImportDataPage from "@/pages/ImportData/ImportDataPage";
-import Dashboard from "@/pages/Dashboard";
+import DashboardPage from "@/pages/Dashboard/DashboardPage";
 import Plugins from "@/pages/Plugins";
 import Settings from "@/pages/Settings";
 
 const routes = [
     { path: '/', component: ImportDataPage },
     { path: '/import-data', component: ImportDataPage },
-    { path: '/dashboard', component: Dashboard },
+    { path: '/dashboard', component: DashboardPage },
     { path: '/plugins', component: Plugins },
     { path: '/settings', component: Settings },
 ]
