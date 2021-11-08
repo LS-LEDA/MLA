7e8b7d70f5726ea9bafc984119ae33f3ebaa1db7
Aleix <aleix.olle@students.salle.url.edu>
2021-11-08 18:32:54 +0100

    #36 feat: little changes

--- src/components/Summary/SummaryCard.vue
+++ src/components/Summary/SummaryCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid grid-cols-2 grid-rows-3 bg-white rounded-3xl w-full ">
-    <span class="ml-3 text-xl text-gray-500 row-start-1 row-end-1 col-start-1 col-end-1">{{ page.statistic_name }}</span>
-    <span class="ml-3 text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1">{{ page.number }}</span>
+    <span class="ml-3 text-xl text-gray-500 row-start-1 row-end-1 col-start-1 col-end-1">{{ statistic.statistic_name }}</span>
+    <span class="ml-3 text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1">{{ statistic.number }}</span>
     <svg-icon class ="row-start-2 row-end-2 col-start-2 col-end-2 justify-self-start mr-10" height="90" width="90"
-        type="mdi" :path="page.icon"></svg-icon>
-    <svg-icon class= "row-start-3 row-end-3 col-start-2 col-end-2 place-self-end mr-3 mb-3" type="mdi" :path=path @click="informationPopUp"></svg-icon>
+        type="mdi" :path="statistic.icon"></svg-icon>
+    <svg-icon class= "row-start-3 row-end-3 col-start-2 col-end-2 place-self-end mr-3 mb-3" type="mdi" :path=path @click="changeView(statistic.id)"></svg-icon>
 
 
   </div>
@@ -16,10 +16,11 @@ import {mdiHelpCircleOutline} from "@mdi/js"
 
 export default {
   name: "SummaryCard",
-  props: ['page'],
+  props: ['statistic'],
   components: {
     SvgIcon
   },
+  emits:['changeView'],
   data() {
     return {
       path: mdiHelpCircleOutline
@@ -27,8 +28,9 @@ export default {
   },
   methods: {
 
-    informationPopUp() {
-      console.log("HOLA")
+    changeView: function (id) {
+      console.log(id)
+      this.$emit('changeView', id)
     }
   }
 }
--- src/pages/Dashboard/Summary/Summary.vue
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -3,7 +3,7 @@
     <span class="font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
     <InteractionCard/>
     <div class="grid grid-rows-2 grid-cols-3 gap-5 bg-transparent w-full h-full bg-white rounded-3xl mt-5">
-      <SummaryCard v-for="(statistic, index) in statistics" :page="statistic" :key="index"/>
+      <SummaryCard v-for="(statistic, index) in statistics" :statistic="statistic" :key="index"/>
     </div>
 </template>
 
@@ -25,36 +25,44 @@ export default {
           statistics:[
             {
               statistic_name: "Tasks",
-              number: "0",
-              icon: mdiFileDocumentOutline
+              number: 0,
+              icon: mdiFileDocumentOutline,
+              id: 0
             },
             {
               statistic_name: "Files",
-              number: "0",
-              icon: mdiFileDocumentOutline
+              number: 0,
+              icon: mdiFileDocumentOutline,
+              id: 1
             },
             {
               statistic_name: "Pages",
-              number:"0",
-              icon: mdiFileDocumentOutline
+              number:0,
+              icon: mdiFileDocumentOutline,
+              id: 2
             },
             {
               statistic_name: "URL",
-              number:"0",
-              icon: mdiFileDocumentOutline
+              number:0,
+              icon: mdiFileDocumentOutline,
+              id: 3
             },
             {
               statistic_name: "Learning Tools Interoperability",
-              number:"0",
-              icon: mdiFileDocumentOutline
+              number:0,
+              icon: mdiFileDocumentOutline,
+              id: 4
             },
             {
               statistic_name: "Wiki",
-              number:"0",
-              icon: mdiFileDocumentOutline
-            },
+              number:0,
+              icon: mdiFileDocumentOutline,
+              id: 5
+            }
           ]
         }
+
+
       }
 }
 </script>
