2e2612a16033018f12f5c51216072dd4ad338eaf
Aleix <aleix.olle@students.salle.url.edu>
2021-11-08 19:40:33 +0100

    #36 feat: Icons changed and resized, cards resized

--- src/components/Summary/SummaryCard.vue
+++ src/components/Summary/SummaryCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid grid-cols-2 grid-rows-3 bg-white rounded-3xl w-full hover:cursor-pointer filter drop-shadow-lg
         transform transition duration-500 hover:scale-[101%]">
-    <span class="ml-3 text-xl text-gray-500 row-start-1 row-end-1 col-start-1 col-end-1" v-if="front">{{ statistic.statistic_name }}</span>
+    <span class="ml-3 mt-3 text-xl text-gray-500 row-start-1 row-end-1 col-start-1 col-end-1" v-if="front">{{ statistic.statistic_name }}</span>
     <span class="ml-3 text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1" v-if="front" >{{ statistic.number }}</span>
-    <svg-icon class ="row-start-2 row-end-2 col-start-2 col-end-2 justify-self-start mr-10" height="90" width="90"
+    <svg-icon class ="row-start-2 row-end-2 col-start-2 col-end-2 justify-self-end mr-20" height="70" width="70"
         type="mdi" :path="statistic.icon" v-if="front"></svg-icon>
     <svg-icon class= "row-start-3 row-end-3 col-start-2 col-end-2 place-self-end mr-3 mb-3" type="mdi" :path=path @click="changeView()"></svg-icon>
     <span  class=" mr-3 ml-3 text-xl font-bold row-start-1 row-end-4 col-start-1 col-end-3 text-center self-center" v-if="back"> {{statistic.info}} </span>
--- src/pages/Dashboard/Summary/Summary.vue
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -2,7 +2,7 @@
     <h1 class="text-4xl font-extrabold"> Summary of all course interactions </h1>
     <span class="font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
     <InteractionCard/>
-    <div class="grid grid-rows-2 grid-cols-3 gap-5 bg-transparent w-full h-full bg-white rounded-3xl mt-5">
+    <div class="grid grid-rows-2 grid-cols-3 gap-x-20 gap-y-10 bg-transparent w-full h-full bg-white rounded-3xl mt-5 ">
       <SummaryCard v-for="(statistic, index) in statistics" :statistic="statistic" :key="index"/>
     </div>
 </template>
@@ -11,7 +11,12 @@
 import InteractionCard from "@/components/Summary/InteractionCard";
 import SummaryCard from "@/components/Summary/SummaryCard";
 import {
-  mdiFileDocumentOutline
+  mdiFileDocumentOutline,
+  mdiTextBoxCheck,
+  mdiHammerScrewdriver,
+  mdiWikipedia,
+  mdiLinkVariant,
+  mdiViewDashboardOutline
 } from "@mdi/js";
 
 export default {
@@ -25,38 +30,38 @@ export default {
           statistics:[
             {
               statistic_name: "Tasks",
-              number: 0,
-              icon: mdiFileDocumentOutline,
+              number: 7342,
+              icon: mdiTextBoxCheck ,
               info:"The total number of interactions with all deliveries of a subject. "
             },
             {
               statistic_name: "Files",
-              number: 0,
+              number: 1913,
               icon: mdiFileDocumentOutline,
               info:" The total number of interactions with all files of a subject."
             },
             {
               statistic_name: "Pages",
-              number:0,
-              icon: mdiFileDocumentOutline,
+              number:75,
+              icon: mdiViewDashboardOutline,
               info:"The total number of interactions with the pages of a subject."
             },
             {
               statistic_name: "URL",
-              number:0,
-              icon: mdiFileDocumentOutline,
+              number:34,
+              icon: mdiLinkVariant ,
               info: "The total number of interactions with the URL resource of a subject."
             },
             {
               statistic_name: "Learning Tools Interoperability",
               number:0,
-              icon: mdiFileDocumentOutline,
+              icon: mdiHammerScrewdriver ,
               info:"The total number of interactions with the learning tools interoperability resources of a subject."
             },
             {
               statistic_name: "Wiki",
-              number:0,
-              icon: mdiFileDocumentOutline,
+              number:1,
+              icon: mdiWikipedia ,
               info:"The total number of interactions with the wikis of a subject."
             }
           ]
