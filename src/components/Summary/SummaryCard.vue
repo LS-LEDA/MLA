230c293833fd76b6999272d9a5f3e8ab27dda8d4
Aleix <aleix.olle@students.salle.url.edu>
2021-11-08 13:09:05 +0100

    #36 feat: Creation of the Summary Card front view, first commit

---
+++ src/components/Summary/SummaryCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-row bg-blue-100 rounded-3xl w-full relative">
+    hola
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "SummaryCard",
+  props: ['page'],
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Dashboard/Summary/Summary.vue
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -2,15 +2,25 @@
     <h1 class="text-4xl font-extrabold"> Summary of all course interactions </h1>
     <span class="font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
     <InteractionCard/>
+    <div class="grid grid-rows-2 grid-cols-3 gap-5 bg-white w-full h-full bg-white rounded-3xl mt-5">
+      <SummaryCard/>
+      <SummaryCard/>
+      <SummaryCard/>
+      <SummaryCard/>
+      <SummaryCard/>
+      <SummaryCard/>
+    </div>
 </template>
 
 <script>
 import InteractionCard from "@/components/Summary/InteractionCard";
+import SummaryCard from "@/components/Summary/SummaryCard";
 
 export default {
     name: "Summary",
     components: {
-        InteractionCard
+        InteractionCard,
+        SummaryCard
     }
 }
 </script>
