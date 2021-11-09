8bc65d31ecc9084dd5078d116dc536b2d5494ace
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-09 17:05:17 +0100

    #37 refactor: Flex layout card back side - Close Icon - Centered text

--- src/components/Summary/SummaryCard.vue
+++ src/components/Summary/SummaryCard.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="flex flex-col w-full bg-white rounded-3xl w-full filter drop-shadow-lg
-    transform transition duration-500 hover:scale-[101%] select-none p-10 pb-5">
+    transform transition duration-500 hover:scale-[101%] select-none p-10 pb-5" :class="card_status ? 'pb-10': null">
         <!-- Card information -->
-        <span class="mr-3 ml-3 text-xl font-bold text-center self-center" v-if="card_status"> {{statistic.info}} </span>
+        <template v-if="card_status">
+            <svg-icon class="items-end place-self-end hover:cursor-pointer text-gray-400" type="mdi" :path=close_icon @click="changeView()"></svg-icon>
+            <span class="flex h-full mx-3 text-xl font-bold text-center self-center place-self-center items-center">
+                {{ statistic.info }}
+            </span>
+        </template>
         <template v-else>
             <!-- Card title -->
-            <span class="text-xl text-gray-500 font-bold row-start-1 row-end-1 col-start-1 col-end-1">{{ statistic.statistic_name }}</span>
+            <span class="text-xl text-gray-500 font-bold row-start-1 row-end-1 col-start-1 col-end-1">
+                {{ statistic.statistic_name }}
+            </span>
             <!-- Card Statistics & Logo -->
             <div class="flex w-full flex-1 justify-between items-center">
-                <span class="text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1" >{{ statistic.number }}</span>
+                <span class="text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1" >
+                    {{ statistic.number }}
+                </span>
                 <svg-icon class ="justify-self-end" height="100" width="100" type="mdi" :path="statistic.icon"></svg-icon>
             </div>
             <!-- Card Information Icon -->
-            <svg-icon class="items-end place-self-end hover:cursor-pointer" type="mdi" :path=path @click="changeView()"></svg-icon>
+            <svg-icon class="items-end place-self-end hover:cursor-pointer" type="mdi" :path=help_icon @click="changeView()"></svg-icon>
         </template>
     </div>
 </template>
 
 <script>
 import SvgIcon from '@jamescoyle/vue-icon'
-import {mdiHelpCircleOutline} from "@mdi/js"
+import {mdiClose, mdiHelpCircleOutline} from "@mdi/js"
 
 export default {
     name: "SummaryCard",
@@ -29,7 +38,8 @@ export default {
     },
     data() {
         return {
-            path: mdiHelpCircleOutline,
+            help_icon: mdiHelpCircleOutline,
+            close_icon: mdiClose,
             /**
              * Cart Status
              * false:   Front
