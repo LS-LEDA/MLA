d58614ebb8e8478059e680d6356c0c2702c230eb
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-11-09 17:23:12 +0100

    Revert "Revert 44 38/feat/summary card"

---
+++ src/pages/Dashboard/Summary/Summary.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="text-4xl font-extrabold"> Summary of all course interactions </h1>
+    <span class="font-bold text-2xl text-gray-500"> Information on the number of interactions </span>
+    <InteractionCard/>
+</template>
+
+<script>
+import InteractionCard from "@/components/Summary/InteractionCard";
+
+export default {
+    name: "Summary",
+    components: {
+        InteractionCard
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ImportDataPage from "@/pages/ImportData/ImportDataPage";
-import Dashboard from "@/pages/Dashboard";
+import DashboardPage from "@/pages/Dashboard/DashboardPage";
 import Plugins from "@/pages/Plugins";
 import Settings from "@/pages/Settings";
+import Summary from "@/pages/Dashboard/Summary/Summary";
 
 const routes = [
     { path: '/', component: ImportDataPage },
     { path: '/import-data', component: ImportDataPage },
-    { path: '/dashboard', component: Dashboard },
+    {
+        path: '/dashboard',
+        component: DashboardPage,
+        children:[
+            {
+                path: '/dashboard/summary',
+                component: Summary
+            }
+        ]
+    },
     { path: '/plugins', component: Plugins },
     { path: '/settings', component: Settings },
 ]
