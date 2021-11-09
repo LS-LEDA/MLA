6db7908f46be3bb665f933aecfa0bc3bf0e356aa
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-09 16:19:20 +0100

    #37 refactor: card state - Card state variable - Vue conditional rendering block

--- src/components/Summary/SummaryCard.vue
+++ src/components/Summary/SummaryCard.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="flex flex-col w-full bg-white rounded-3xl w-full filter drop-shadow-lg
     transform transition duration-500 hover:scale-[101%] select-none p-10 pb-5">
-        <!-- Card title -->
-        <span class="text-xl text-gray-500 font-bold row-start-1 row-end-1 col-start-1 col-end-1" v-if="front">{{ statistic.statistic_name }}</span>
-        <!-- Card Statistics & Logo -->
-        <div class="flex w-full flex-1 justify-between items-center">
-            <span class="text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1" v-if="front" >{{ statistic.number }}</span>
-            <svg-icon class ="justify-self-end" height="100" width="100" type="mdi" :path="statistic.icon" v-if="front"></svg-icon>
-        </div>
-        <!-- Card Information -->
-        <svg-icon class="items-end place-self-end hover:cursor-pointer" type="mdi" :path=path @click="changeView()"></svg-icon>
-        <span class="mr-3 ml-3 text-xl font-bold text-center self-center" v-if="back"> {{statistic.info}} </span>
+        <!-- Card information -->
+        <span class="mr-3 ml-3 text-xl font-bold text-center self-center" v-if="card_status"> {{statistic.info}} </span>
+        <template v-else>
+            <!-- Card title -->
+            <span class="text-xl text-gray-500 font-bold row-start-1 row-end-1 col-start-1 col-end-1">{{ statistic.statistic_name }}</span>
+            <!-- Card Statistics & Logo -->
+            <div class="flex w-full flex-1 justify-between items-center">
+                <span class="text-6xl font-bold row-start-2 row-end-2 col-start-1 col-end-1" >{{ statistic.number }}</span>
+                <svg-icon class ="justify-self-end" height="100" width="100" type="mdi" :path="statistic.icon"></svg-icon>
+            </div>
+            <!-- Card Information Icon -->
+            <svg-icon class="items-end place-self-end hover:cursor-pointer" type="mdi" :path=path @click="changeView()"></svg-icon>
+        </template>
     </div>
 </template>
 
@@ -27,14 +30,17 @@ export default {
     data() {
         return {
             path: mdiHelpCircleOutline,
-            back: false,
-            front: true
+            /**
+             * Cart Status
+             * false:   Front
+             * true:    Back
+             */
+            card_status: false
         }
     },
     methods: {
         changeView: function () {
-            this.back = !this.back
-            this.front = !this.front
+            this.card_status = !this.card_status
         }
     }
 }
