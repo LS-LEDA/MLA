392918149bb7afbf8d35649e02b2750a033aa9bb
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-11 18:29:08 +0100

    #50 feat: change nav bar status & nav bar header routing

--- src/components/NavigationBar/NavigationHeader.vue
+++ src/components/NavigationBar/NavigationHeader.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="flex flex-row py-2 mx-2 my-5 h-20">
-        <div class="flex-initial">
-            <img class="w-auto h-full rounded-lg"
-                 src="/assets/jsmla_logo.png" alt="jsMLA Logo">
+        <!-- Navigation bar App logo routed -->
+        <router-link to="/import-data" :class="nav_state ? null : 'relative'">
+            <img class="h-full w-full rounded-lg" src="/assets/jsmla_logo.png" alt="jsMLA Logo">
+        </router-link>
+        <!-- Application name -->
+        <div class="flex-initial mx-4 font-bold text-2xl self-center hover:cursor-pointer" v-if="nav_state">
+            <router-link to="/import-data">
+                <h1>jsMLA</h1>
+            </router-link>
         </div>
-        <div class="flex-initial mx-4 font-bold text-4xl self-center">
-            <h1>jsMLA</h1>
-        </div>
-        <div class="flex flex-1 w-max justify-end">
-            <button type="button" class="self-center flex flex-row justify-self-end">
-                <svg-icon size="36" type="mdi" :path="path" class="hover:drop-shadow shrink_icon"></svg-icon>
+        <!-- Navigation shrink button -->
+        <div class="flex flex-1 w-max justify-end" :class="nav_state ? null : 'absolute'">
+            <button type="button" class="self-center flex flex-row justify-self-end" @click="changeNavigationBarStatus"
+                    :class="nav_state ? null : 'transform transition duration-500 rotate-180'">
+                <svg-icon size="36" type="mdi" :path="path" class="hover:drop-shadow shrink_icon"/>
             </button>
         </div>
     </div>
@@ -33,6 +38,12 @@ export default {
     data() {
         return {
             path: mdiChevronDoubleLeft
+        }
+    },
+    methods: {
+        // Expand or shrink navigation bar
+        changeNavigationBarStatus() {
+            this.$store.commit('changeNavigationBarStatus')
         }
     }
 }
