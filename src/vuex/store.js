d6a300e441a48f2636bcb70bc83235188615888a
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-11 18:46:45 +0100

    #50 feat: Imported data status - Method to set status

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,13 +8,24 @@ const store = createStore({
              * true:    Expanded
              * false:   Shrank
              */
-            navigation_bar_status: false
+            navigation_bar_status: false,
+            /**
+             * This value will determine where the navigation
+             * header will redirect to:
+             * If there's uploaded data Dashboard page is
+             * rendered, otherwise Import Data page
+             */
+            imported_data: false
         }
     },
     mutations: {
         // Expand or shrink navigation bar
         changeNavigationBarStatus(){
             this.state.navigation_bar_status = !this.state.navigation_bar_status
+        },
+        // Call this method if data has been uploaded
+        setImportedData() {
+            this.state.imported_data = true
         }
     }
 });
