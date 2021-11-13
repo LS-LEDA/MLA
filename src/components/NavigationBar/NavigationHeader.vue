b211474db1dca4ff93b35d0ea519d6e2b332a30b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-13 20:23:06 +0100

    #50 feat: nav bar logo show expand button on hover

--- src/components/NavigationBar/NavigationHeader.vue
+++ src/components/NavigationBar/NavigationHeader.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="flex flex-row py-2 mx-2 my-5 h-20" :class="nav_state ? null : 'justify-center items-center'">
         <!-- Navigation bar App logo routed -->
-        <router-link :to="imported_data ? '/dashboard' : '/import-data'">
-                <img class="max-h-16 rounded-lg origin-center z-50"
-                     :class="'z-50'"
-                     src="/assets/jsmla_logo.png" alt="jsMLA Logo">
-        </router-link>
+        <div :class="logo_hovered ? 'z-0' : 'z-50'">
+            <router-link :to="imported_data ? '/dashboard' : '/import-data'"
+                         :class="logo_hovered ? 'invisible' : null"
+                         @mouseover="logo_hover"
+                         @mouseleave="logo_not_hover">
+                    <img class="max-h-16 rounded-lg origin-center transform transition duration-500"
+                         src="/assets/jsmla_logo.png" alt="jsMLA Logo">
+            </router-link>
+        </div>
         <!-- Application name -->
         <!--<transition name="fade">-->
         <div class="flex-1 mx-4 font-bold text-2xl self-center hover:cursor-pointer" v-if="nav_state">
@@ -15,9 +19,11 @@
         </div>
         <!--</transition>-->
         <!-- Navigation shrink button -->
-        <button type="button" class="flex self-center center w-auto place-items-center z-0"
+        <button type="button" class="flex self-center center w-auto place-items-center"
                 :class="nav_state ? null : 'absolute transform transition duration-700 rotate-180'"
-                @click="changeNavigationBarStatus">
+                @click="changeNavigationBarStatus"
+                @mouseover="logo_hover"
+                @mouseleave="logo_not_hover">
             <svg-icon size="36" type="mdi" :path="path" class="hover:drop-shadow shrink_icon place-self-center"/>
         </button>
     </div>
@@ -43,13 +49,22 @@ export default {
     },
     data() {
         return {
-            path: mdiChevronDoubleLeft
+            path: mdiChevronDoubleLeft,
+            logo_hovered: false
         }
     },
     methods: {
         // Expand or shrink navigation bar
         changeNavigationBarStatus() {
             this.$store.commit('changeNavigationBarStatus')
+        },
+        logo_hover() {
+            if(!this.nav_state) {
+                this.logo_hovered = true
+            }
+        },
+        logo_not_hover() {
+            this.logo_hovered = false
         }
     }
 }
