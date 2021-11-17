153b3d717f1002fa0a2da92c9d91501338fdde20
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-17 16:15:47 +0100

    #57 feat: sentiment analysis page layout

---
+++ src/pages/Dashboard/Sentimental/Sentimental.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="text-4xl font-extrabold"> Sentimental Analysis </h1>
+    <span class="font-bold text-2xl text-gray-500"> Forum messages sentiments </span>
+</template>
+
+<script>
+export default {
+    name: "Sentimental"
+}
+</script>
+
+<style scoped>
+
+</style>
