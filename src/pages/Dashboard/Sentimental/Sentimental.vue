2d83898fe14ff0b42305609fc156847f8d04b149
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-19 17:08:33 +0100

    #67 refactor: computed forum messages from vuex - Removed mock forum messages

--- src/pages/Dashboard/Sentimental/Sentimental.vue
+++ src/pages/Dashboard/Sentimental/Sentimental.vue
@@ -26,31 +26,9 @@ export default {
         SentimentScore,
         SentimentChatCard
     },
-    data() {
-        return {
-            forum_messages: [
-                {
-                    userfullname: "User 1",
-                    message: "I love cats 🐈",
-                    timestamp: "18/11/2021"
-                },
-                {
-                    userfullname: "User 2",
-                    message: "I love cats too🐈  I own a cat called Maro, what about you guys? Do you have any pets at home?" +
-                        "I love cats too🐈  I own a cat called Maro, what about you guys? Do you have any pets at home?",
-                    timestamp: "18/11/2021"
-                },
-                {
-                    userfullname: "User 3",
-                    message: "Yes I do, but I’m more of a dog 🐕 fan. My cute little doggie’s name is Cheems",
-                    timestamp: "18/11/2021"
-                },
-                {
-                    userfullname: "User 4",
-                    message: "You bastards, free those animals, they’re not toys!!",
-                    timestamp: "18/11/2021"
-                }
-            ]
+    computed: {
+        forum_messages() {
+            return this.$store.state.forum_messages
         }
     }
 }
