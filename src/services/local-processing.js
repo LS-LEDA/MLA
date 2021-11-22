6cf239efca18b5499ec4829cf9fa6b0499582553
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-22 13:14:19 +0100

    #67 feat: guess & detect sentiment of messages

--- src/services/local-processing.js
+++ src/services/local-processing.js
@@ -6,6 +6,16 @@ const SUPPORTED_FILE_TYPE   = 2
 
 const file_reader = new FileReader()
 
+
+const { Container } = require('@nlpjs/core');
+const { SentimentAnalyzer } = require('@nlpjs/sentiment');
+const { LangCa } = require('@nlpjs/lang-ca');
+const { LangEs } = require('@nlpjs/lang-es');
+const {Language} = require('@nlpjs/language/src/language');
+
+// NLP
+const lng_guesser = new Language();
+
 function local_processing(file) {
 
     // Check if it's a JSON log file
@@ -18,6 +28,8 @@ function local_processing(file) {
         let data = JSON.parse(event.target.result)
 
         let messages = data[0]
+
+        analyze_sentiment(messages).then()
 
         // Store message in vuex
         store.commit('storeForumMessages', messages)
@@ -34,4 +46,28 @@ function check_file_type(filetype) {
     return SUPPORTED_FILE_TYPE
 }
 
+async function analyze_sentiment(messages) {
+    // Compute sentiment
+    messages.map( async (msg) => {
+        // Guess the language of the message
+        let lng_guess = lng_guesser.guess(msg.message)
+        console.log(lng_guess)
+
+        const container = new Container();
+
+        switch (lng_guess[0].alpha2) {
+            case 'es':
+                container.use( LangEs );
+                break;
+            case 'ca':
+                container.use( LangCa );
+                break;
+        }
+
+        const sentiment = new SentimentAnalyzer({ container });
+
+        const result = await sentiment.process({ locale: lng_guess[0].alpha2, text: msg.message });
+        console.log(result)
+    })
+}
 export { local_processing }
