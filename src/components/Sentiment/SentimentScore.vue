d7d5f3e778cdbd0e0d705d2b7827e5b56e2d0752
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-24 17:33:07 +0100

    #67 refactor: support for neutral sentiment - Style

--- src/components/Sentiment/SentimentScore.vue
+++ src/components/Sentiment/SentimentScore.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex w-full h-20 border-2 rounded-xl place-content-center items-center"
-         :class="selectScore ? 'bg-green-300 border-green-500' : 'bg-red-300 border-red-500'">
+         :class="selectScore">
         <span class="font-black text-white text-5xl"> {{ score }} </span>
     </div>
 </template>
@@ -14,10 +14,17 @@ export default {
          * Determine if the selected message
          * is positive or not, hence, it changes
          * the badge styles
-         * @returns {boolean}
+         * @returns {string}
          */
         selectScore() {
-            return this.score === "POSITIVE"
+            switch (this.score){
+                case "positive":
+                    return 'bg-green-300 border-green-500';
+                case "negative":
+                    return 'bg-red-300 border-red-500';
+                default:
+                    return 'bg-yellow-300 border-yellow-500';
+            }
         }
     }
 }
