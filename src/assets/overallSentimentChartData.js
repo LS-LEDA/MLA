16d8ecf3ae300367ed732abe989643b3c7836ce5
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-25 19:48:16 +0100

    #67 feat: support for neutral sentiment data

--- src/assets/overallSentimentChartData.js
+++ src/assets/overallSentimentChartData.js
@@ -3,17 +3,20 @@ export const overallSentimentChartData = {
     data: {
         labels: [
             "Positive",
+            "Neutral",
             "Negative",
         ],
         datasets: [{
             label: '',
-            data: [3, 1],
+            data: [3, 2, 1],
             backgroundColor: [
                 'rgba(66, 196, 158, 0.6)',
+                'rgba(233, 196, 106, 0.6)',
                 'rgba(225, 86, 86, 0.6)',
             ],
             borderColor: [
                 'rgba(66, 196, 158, 1)',
+                'rgba(233, 196, 106, 1)',
                 'rgba(225, 86, 86, 1)',
             ],
             borderWidth: 2
