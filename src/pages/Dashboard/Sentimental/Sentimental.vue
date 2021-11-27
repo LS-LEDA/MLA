245d6fd1a6359bff3e122d6e8bb9bbf7d7165bd0
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-27 11:06:10 +0100

    #67 refactor: sentiment tab grid layout to flexbox

--- src/pages/Dashboard/Sentimental/Sentimental.vue
+++ src/pages/Dashboard/Sentimental/Sentimental.vue
@@ -2,23 +2,30 @@
     <h1 class="text-4xl font-extrabold"> Sentimental Analysis </h1>
     <span class="font-bold text-2xl text-gray-500"> Forum messages sentiments </span>
 
-    <div class="grid grid-rows-4 grid-cols-2 w-full h-full gap-5 mt-5">
-        <SentimentFileCard class="row-span-1"
-                           :file_name="selected_file_name"
-                           :messages="forum.messages"
-                           :users="forum.users"/>
-        <SentimentOverallCard :sentiments="forum.sentiments"/>
-        <div class="flex flex-col h-full bg-white rounded-xl row-span-3 p-10 overflow-y-scroll overflow-x-hidden gap-y-5
-                    backdrop-filter">
-            <SentimentChatCard v-for="(message, index) in forum.forum_messages" :messages="message" :key="index"
-                               :class="selected_id === index ? 'brightness-100' : 'brightness-75'"
-                               @click="select_msg(index)"/>
+    <div class="flex flex-col w-full h-full mt-5 gap-y-5">
+        <!-- Sentiment cards -->
+        <div class="flex w-full h-52 gap-x-5">
+            <SentimentFileCard class="row-span-1"
+                               :file_name="selected_file_name"
+                               :messages="forum.messages"
+                               :users="forum.users"/>
+            <SentimentOverallCard :sentiments="forum.sentiments"/>
         </div>
-        <SentimentScore :score="selected_msg_score"/>
+
+        <div class="flex w-full h-full gap-x-5 pb-10">
+            <div class="flex flex-col w-1/2 bg-white rounded-xl p-10 overflow-y-scroll overflow-x-hidden gap-y-5
+                        backdrop-filter">
+                <SentimentChatCard v-for="(message, index) in forum.forum_messages" :messages="message" :key="index"
+                                   :class="selected_id === index ? 'brightness-100' : 'brightness-75'"
+                                   @click="select_msg(index)"/>
+            </div>
+            <SentimentScore class="w-1/2" :score="selected_msg_score"/>
+        </div>
     </div>
 </template>
 
 <script>
+/* eslint-disable */
 import SentimentChatCard from "@/components/Sentiment/SentimentChatCard";
 import SentimentScore from "@/components/Sentiment/SentimentScore";
 import SentimentOverallCard from "@/components/Sentiment/SentimentOverallCard";
@@ -27,6 +34,7 @@ import SentimentFileCard from "@/components/Sentiment/SentimentFileCard";
 export default {
     name: "Sentimental",
     components: {
+
         SentimentFileCard,
         SentimentOverallCard,
         SentimentScore,
