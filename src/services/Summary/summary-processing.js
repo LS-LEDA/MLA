ee23caeab8732484d0a0f1f507f75b9188789787
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-30 16:29:07 +0100

    #75 feat: summary type count & categories count

--- src/services/Summary/summary-processing.js
+++ src/services/Summary/summary-processing.js
@@ -1,29 +1,20 @@
 function summary_processing(logs) {
-    let tasks_count =   0;
-    let files_count =   0;
-    let pages_count =   0;
-    let url_count   =   0;
-    let lti_count   =   0;
-    let wiki_count  =   0;
-
-    let task_type = {};
     let type = {};
     logs.forEach( (log) => {
-
-        type[log.type] = (type[log.type] + 1) || 1;
-        /*switch (log.type) {
-            case 'Tarea':
-            case 'Tarea':
-            case 'Tarea':
-            case 'Tarea':
-            case 'Tarea':
-            case 'Tarea':
-        }*/
-        if ( log.type === 'Tarea') {
-            tasks_count++;
-            task_type[log.interaction] = (task_type[log.interaction] + 1) || 1;
+        // Process all types of logs and count each one of them
+        if ( type[log.type] !== undefined ) {
+            type[log.type].count +=1;
+            type[log.type].interactions[log.interaction] = (type[log.type].interactions[log.interaction] + 1) || 1;
+        } else {
+            type[log.type] = {
+                count: 1,
+                interactions: {}
+            }
+            type[log.type].interactions[log.interaction] = 1;
         }
     })
+
+    return type;
 }
 
 export { summary_processing };
