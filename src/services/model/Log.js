5959ddb6c281fc22fdedca8fe1a0378eda9439f1
JiahuiChen99 <Jiahui1@hotmail.es>
2021-11-30 17:54:36 +0100

    #74 feat: log struct yearMonthDate property

--- src/services/model/Log.js
+++ src/services/model/Log.js
@@ -1,6 +1,7 @@
 class Log {
     constructor(date, id, name, course, type, interaction, client, ip) {
         this.date = date;
+        this.yearMonthDate = this.year_month_date()
         this.id = id;
         this.name = name;
         this.course = course;
@@ -9,6 +10,13 @@ class Log {
         this.client = client;
         this.ip = ip;
     }
+
+    year_month_date() {
+        return  this.date.substr(6, 4) +
+                this.date.substr(3, 2) +
+                this.date.substr(0, 2);
+    }
 }
+
 
 export default Log;
