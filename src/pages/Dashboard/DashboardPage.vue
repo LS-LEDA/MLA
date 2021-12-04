ebb3f5acea3d546f14df82cfce6e58b1cac5f707
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-04 13:38:12 +0100

    #80 refactor: dashboard layout support for summary popup

--- src/pages/Dashboard/DashboardPage.vue
+++ src/pages/Dashboard/DashboardPage.vue
@@ -1,13 +1,16 @@
 <template>
-    <section id="dashboard" class="flex flex-col w-screen h-screen p-10 overflow-y-auto">
-        <!-- Tabs buttons -->
-        <div class="flex flex-row pb-5 space-x-5">
-            <router-link :to="tab.tab_path" v-for="(tab, index) in tabs" :key="index" class="">
-                <button class="bg-blue-100 hover:bg-blue-200 rounded-lg text-2xl font-bold py-3 px-10"> {{ tab.tab_name }}</button>
-            </router-link>
+    <section id="dashboard" class="relative flex flex-col w-full h-full">
+        <div class="flex flex-col w-full h-full p-10 overflow-y-auto">
+            <!-- Tabs buttons -->
+            <div class="flex flex-row pb-5 space-x-5">
+                <router-link :to="tab.tab_path" v-for="(tab, index) in tabs" :key="index" class="">
+                    <button class="bg-blue-100 hover:bg-blue-200 rounded-lg text-2xl font-bold py-3 px-10"> {{ tab.tab_name }}</button>
+                </router-link>
+            </div>
+            <!-- Dashboard deeply nested component view -->
+            <router-view @popUp="toggle_popup()"></router-view>
         </div>
-        <!-- Dashboard deeply nested component view -->
-        <router-view></router-view>
+        <SummaryPopUp v-if="popup" @popUp="toggle_popup" class="absolute"/>
     </section>
 </template>
 
