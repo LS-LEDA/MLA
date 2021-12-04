8faa88c1d26842e6dc7a545842a571714b1722d4
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-04 16:49:21 +0100

    #80 feat: summary popup basic layout - PopUp title - Chart section - Table section

--- src/components/Summary/SummaryPopUp.vue
+++ src/components/Summary/SummaryPopUp.vue
@@ -1,12 +1,46 @@
 <template>
     <div class="flex flex-col absolute w-full h-full justify-center place-items-center bg-gray-400 bg-opacity-75 transition-opacity"
          role="dialog" aria-modal="true">
-        <div class="flex flex-col bg-white w-11/12 h-5/6 rounded-xl z-50">
-            <button type="button" class="bg-gray-100 text-gray-400 rounded-lg focus:ring-2 focus:ring-gray-200
-            hover:bg-gray-200 inline-flex hover:bg-blue-500 rounded-xl w-10 h-10 place-content-center items-center"
-                    @click="close_pop_up">
-                <SvgIcon class="hover:cursor-pointer" type="mdi" :path="close_icon"/>
-            </button>
+
+        <!-- PopUp white background -->
+        <div class="flex flex-col bg-white w-11/12 h-5/6 rounded-xl z-50 p-5">
+            <!-- Close button -->
+            <div class="flex w-full">
+                <!-- PopUp Title -->
+                <div class="w-full">
+
+                </div>
+                <button type="button" class=" justify-self-end bg-gray-100 text-gray-400 rounded-lg focus:ring-2 focus:ring-gray-200
+                hover:bg-gray-200 inline-flex hover:bg-blue-500 rounded-xl w-10 h-10 place-content-center items-center"
+                        @click="close_pop_up">
+                    <SvgIcon class="hover:cursor-pointer" type="mdi" :path="close_icon"/>
+                </button>
+            </div>
+
+            <!-- Summary card chart -->
+            <div class="flex w-full h-3/6">
+            </div>
+            <!-- Summary card table -->
+            <div class="flex w-full h-full justify-center overflow-y-scroll">
+                <table class="border border-4">
+                    <thead class="">
+                        <tr>
+                            <th> Interactions type </th>
+                            <th> Number of interactions </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="">
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr class="" v-for="index in 100" :key="index">
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="absolute w-full h-full filter backdrop-blur-sm z-10"></div>
     </div>
