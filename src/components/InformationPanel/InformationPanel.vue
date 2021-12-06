16272323041600be965ae1bbc3b1888565021c5e
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-06 13:52:48 +0100

    #85 feat: information panel basic layout

---
+++ src/components/InformationPanel/InformationPanel.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex h-full bg-white w-80 mx-2 my-2 rounded-xl">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InformationPanel"
+}
+</script>
+
+<style scoped>
+
+</style>
