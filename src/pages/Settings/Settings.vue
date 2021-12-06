e5182fca7bb7bc2667d5e296945a4a940c72d188
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-06 20:51:06 +0100

    #85 feat: settings page title & white card

---
+++ src/pages/Settings/Settings.vue
@@ -0,0 +1,20 @@
+<template>
+    <section id="settings" class="relative flex flex-col w-full h-full">
+        <div class="flex flex-col w-full h-full p-10">
+            <h1 class="text-4xl font-extrabold pb-5"> Settings </h1>
+            <div class="w-full h-full bg-white rounded-2xl">
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
