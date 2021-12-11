ce615a87a260dd0077bedff4b5d2834402aff683
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-11 12:59:21 +0100

    #86 chore: electron builder configuration moved to vue config file

---
+++ vue.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                productName: "MLA",
+                appId: "org.gretel.mla",
+                directories: {
+                    "output": "build"
+                },
+                win: {
+                    icon: "resources/installer/icon.ico",
+                    target: [
+                        "zip",
+                        "nsis",
+                        "portable"
+                    ]
+                },
+                nsis: {
+                    oneClick: false,
+                    perMachine: true,
+                    allowElevation: true,
+                    allowToChangeInstallationDirectory: true,
+                    installerIcon: "resources/installer/icon.ico"
+                },
+                mac: {
+                    category: "public.app-category.education",
+                    icon: "resources/installer/icon.ico",
+                    target: [
+                        "zip",
+                        "dmg"
+                    ]
+                },
+                dmg: {
+                    icon: "resources/installer/icon.ico"
+                }
+            }
+        }
+    }
+}
