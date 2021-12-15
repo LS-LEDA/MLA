3b00f52a2fabded0e2e2e49b6c68cdda6a2c6560
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-15 20:34:21 +0100

    #85 feat: Information panel component

--- src/components/InformationPanel/InformationPanel.vue
+++ src/components/InformationPanel/InformationPanel.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="flex h-full bg-white w-80 mx-2 my-2 rounded-xl">
+    <div class="flex h-full w-96 overflow-y-hidden pb-4">
+        <div class="bg-white h-full w-full rounded-lg mx-2 my-2">
+            <Calendar/>
+        </div>
     </div>
 </template>
 
 <script>
+import Calendar from "@/components/UI/Calendar";
 export default {
-    name: "InformationPanel"
+    name: "InformationPanel",
+    components: {Calendar}
 }
 </script>
 
