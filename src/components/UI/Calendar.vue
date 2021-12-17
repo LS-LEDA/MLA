45028f82853181dfb5c269f11df8e24930f4b93b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-17 20:39:39 +0100

    #85 feat: calendar current month days generation

--- src/components/UI/Calendar.vue
+++ src/components/UI/Calendar.vue
@@ -123,7 +123,69 @@
 
 <script>
 export default {
-    name: "Calendar"
+    name: "Calendar",
+    data() {
+        return {
+
+        }
+    },
+    setup() {
+        const date = new Date();
+        date.setDate(1);
+        let formatted_days =  [[], [], [], [], [], []];
+        let days = [];
+
+        const last_day = new Date(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            0
+        ).getDate();
+
+        const prev_last_day = new Date(
+            date.getFullYear(),
+            date.getMonth(),
+            0
+        ).getDate();
+
+        // Position of the month's first day
+        // [M,T,W,T,F,S,S]
+        // [1,2,3,4,5,6,7]
+        const first_day_index = date.getDay() - 1;
+
+        const last_day_index = new Date(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            0
+        ).getDay();
+
+        const next_days = 7 - last_day_index;
+
+        // Previous month days
+        for( let i = first_day_index; i > 0;  i-- ) {
+            days.push(prev_last_day - i + 1);
+        }
+
+        // Current month days
+        for( let i = 1; i <= last_day; i++ ) {
+            //days[ Math.trunc(i / 7)].push(i);
+            days.push(i)
+        }
+
+        // Next month days
+        for ( let j = 1; j <= next_days; j++ ) {
+            days.push(j);
+        }
+
+        let chunks = 0;
+        for ( let i = 0; i < days.length; i += 7) {
+            formatted_days[chunks] = days.slice(i, i + 7);
+            chunks++;
+        }
+
+        return { formatted_days }
+    },
+    methods: {
+    }
 }
 </script>
 
