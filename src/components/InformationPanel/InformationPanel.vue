6b2dbfe994276d86e91fa01f6fbb3114a6f65e4b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-22 18:31:54 +0100

    #85 feat: calendar selected date callback

--- src/components/InformationPanel/InformationPanel.vue
+++ src/components/InformationPanel/InformationPanel.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="h-full w-96 overflow-y-hidden pb-4">
         <div class="flex flex-col bg-white h-full rounded-lg mx-2 my-2">
-            <Calendar/>
+            <Calendar @dateSelect="selectDate" calendarID="1"/>
+            <Calendar @dateSelect="selectDate" calendarID="2"/>
         </div>
     </div>
 </template>
@@ -10,7 +11,31 @@
 import Calendar from "@/components/UI/Calendar";
 export default {
     name: "InformationPanel",
-    components: {Calendar}
+    components: {Calendar},
+    data() {
+        return {
+            start_date: new Date(),
+            end_date: new Date(),
+        }
+    },
+    methods: {
+        /**
+         * Stores the selected dates from the side panels' calendars.
+         * @param date Selected date information { day, month, year }
+         * @param calendarID Which calendar
+         */
+        selectDate: function (date, calendarID) {
+            if ( calendarID === 1) {
+                this.start_date.setDate(date.day);
+                this.start_date.setMonth(date.month);
+                this.start_date.setFullYear(date.year);
+            } else {
+                this.end_date.setDate(date.day);
+                this.end_date.setMonth(date.month);
+                this.end_date.setFullYear(date.year);
+            }
+        }
+    }
 }
 </script>
 
