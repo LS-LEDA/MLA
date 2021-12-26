8734c1a107d70b1e3e6df6f00cad1c2e4a997c2d
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-26 15:05:37 +0100

    #94 refactor: dashboard tabs active style

--- src/pages/Dashboard/DashboardPage.vue
+++ src/pages/Dashboard/DashboardPage.vue
@@ -3,10 +3,13 @@
              :class="popup ? 'overflow-y-hidden' : 'overflow-y-auto'">
         <div class="flex flex-col flex-auto w-full min-h-min px-5 py-10">
             <!-- Tabs buttons -->
-            <div class="grid grid-cols-2 gap-2 xl:flex xl:flex-row pb-5 lg:space-x-5">
-                <router-link :to="tab.tab_path" v-for="(tab, index) in tabs" :key="index" class="">
-                    <button class="w-full bg-blue-100 hover:bg-blue-300 rounded-lg text-2xl font-bold py-3
-                                   lg:px-5 lg:px-10"> {{ tab.tab_name }}</button>
+            <div class="grid grid-cols-2 gap-2 xl:flex xl:flex-row pb-5">
+                <router-link active-class="bg-blue-300"
+                             class="w-full bg-blue-100 hover:bg-blue-300 rounded-lg text-2xl font-bold py-3 text-center"
+                             :to="tab.tab_path"
+                             v-for="(tab, index) in tabs"
+                             :key="index">
+                        {{ tab.tab_name }}
                 </router-link>
             </div>
             <!-- Dashboard deeply nested component view -->
