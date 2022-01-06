76a46696abaef8bc36bc9bda7a5a1cd654463af0
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-06 22:35:50 +0000

    #94 chore: linux distros build target

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,64 @@ module.exports = {
                 },
                 dmg: {
                     icon: "resources/installer/icon.png"
-                }
+                },
+                linux: {
+                    executableName: "MLA",
+                    artifactName: "${productName}-${version}-linux-${arch}.${ext}",
+                    maintainer: "Jiahui Chen",
+                    synopsis: "MLA Is a desktop app for learning analytics",
+                    description: "Moodle Learning analytics is a desktop app that analyses Moodle generated logs and provides insightful information",
+                    category: "Education",
+                    desktop: {
+                        Terminal: "false",
+                        Type: "Application",
+                        Categories: "GTK;GNOME;Education;"
+                    },
+                    target: [
+                        {
+                            target: "zip",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "AppImage",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "deb",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "snap",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "pacman",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "freebsd",
+                            arch: [
+                                "x64"
+                            ]
+                        },
+                        {
+                            target: "rpm",
+                            arch: [
+                                "x64"
+                            ]
+                        }
+                    ]
+                },
             }
         }
     }
