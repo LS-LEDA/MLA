dd621975739a987e7bc4311a34a95be4dca15abe
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-14 20:46:55 +0000

    #104 feat: contributor badge layout with profile icon

--- src/components/UI/ContributorBadge.vue
+++ src/components/UI/ContributorBadge.vue
@@ -1,19 +1,23 @@
 <template>
     <!-- Contributor badge background -->
-    <div class="flex flex-col bg-blue-100 rounded-lg w-full max-h-32 drop-shadow-lg p-5
-                hover:scale-[101%]">
+    <div class="flex bg-blue-100 rounded-lg w-full max-h-32 drop-shadow-lg p-5 hover:scale-[101%] gap-x-3">
         <!-- Profile Icon -->
-        <icon path=""/>
+        <img class="w-20 h-20 rounded-full" :src="contributor.icon" alt="icon">
 
-        <!-- Contributor name -->
-        <div class="font-black">
-            {{ contributor.name }}
+        <!-- Profile information -->
+        <div class="flex flex-col w-full h-full">
+            <!-- Contributor name -->
+            <div class="font-black">
+                {{ contributor.name }}
+            </div>
+
+            <!-- Contributor contact information -->
+            <span class="font-bold text-blue-600 inline">
+                <a href="mailto:jiahui1@hotmail.es">
+                    {{ contributor.email }}
+                </a>
+            </span>
         </div>
-
-        <!-- Contributor contact information -->
-        <a href="mailto:jiahui1@hotmail.es" class="font-bold text-blue-600">
-            {{ contributor.email }}
-        </a>
     </div>
 </template>
 
