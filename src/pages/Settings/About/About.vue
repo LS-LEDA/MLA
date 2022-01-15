f9d3eb5e679f80de20bcf955846f1a8990d81a2e
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-15 18:04:17 +0000

    #109 feat: settings about application information layout

--- src/pages/Settings/About/About.vue
+++ src/pages/Settings/About/About.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="flex w-full h-full">
         <!-- Application information -->
-        <div class="flex flex-col">
+        <div class="flex flex-col w-full lg:w-2/6 h-full place-items-center justify-center">
             <!-- Application logo -->
-            <img :src="mla_logo" alt="MLA logo">
+            <img class="h-32 w-32 my-5" :src="mla_logo" alt="MLA logo">
+            <!-- Application system information-->
+            <div class="flex flex-col space-y-1 place-items-center my-5">
+                <div v-for="(info, index) in information" :key="index">
+                    {{ info }}
+                </div>
+            </div>
+            <!-- External links -->
+            <div class="flex flex-col place-items-center">
+                <div v-for="(link, index) in external_links" :key="index">
+                    {{ link.name }}
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +28,29 @@ export default {
     data() {
         return {
             mla_logo: mla_logo,
+            information: [
+                "MLA for Desktop",
+                "Version 1.0.1 - Andromeda",
+                "Linux x86_64"
+            ],
+            external_links: [
+                {
+                    name: "Website",
+                    link: "https://ls-leda.github.io/Moodle-Learning-Analytics",
+                },
+                {
+                    name: "Changelog",
+                    link: "https://github.com/LS-LEDA/MLA/releases",
+                },
+                {
+                    name: "Documentation",
+                    link: "https://ls-leda.github.io/Moodle-Learning-Analytics/",
+                },
+                {
+                    name: "Report an Issue",
+                    link: "https://github.com/LS-LEDA/MLA/issues",
+                }
+            ]
         }
     }
 }
