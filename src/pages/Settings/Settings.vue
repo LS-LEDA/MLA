246279589d5d3c7a5a3c6971b57cb31e667068c9
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-15 14:53:17 +0000

    #105 feat: settings tabs

--- src/pages/Settings/Settings.vue
+++ src/pages/Settings/Settings.vue
@@ -3,6 +3,17 @@
         <div class="flex flex-col w-full h-full p-10">
             <h1 class="text-4xl font-extrabold pb-5"> Settings </h1>
             <div class="w-full h-full bg-white rounded-2xl">
+                <!-- Tabs buttons -->
+                <div class="grid grid-cols-2 gap-2 xl:flex xl:flex-row pb-5">
+                    <router-link active-class="bg-blue-300"
+                                 class="bg-blue-200 w-full hover:bg-blue-300 rounded-lg text-2xl font-bold py-3 text-center"
+                                 :to="tab.tab_path"
+                                 v-for="(tab, index) in tabs"
+                                 :key="index">
+                        {{ tab.tab_name }}
+                    </router-link>
+                </div>
+
                 <!-- Contributors section -->
 
                 <div class="font-bold text-2xl"> Contributors </div>
@@ -28,6 +39,20 @@ export default {
     components: {ContributorBadge},
     data() {
         return {
+            tabs: [
+                {
+                    tab_name: "General",
+                    tab_path: "/settings/general"
+                },
+                {
+                    tab_name: "Themes",
+                    tab_path: "/settings/themes"
+                },
+                {
+                    tab_name: "About",
+                    tab_path: "/settings/about"
+                }
+            ],
             contributors: [
                 {
                     name: "PhD. Daniel Amo Filvà",
