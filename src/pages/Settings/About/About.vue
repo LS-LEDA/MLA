a094b855d16d5c3c115c37d2d0cc53778ff830cc
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-15 15:40:18 +0000

    #109 feat: application information - mla logo

--- src/pages/Settings/About/About.vue
+++ src/pages/Settings/About/About.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-        About
+    <div class="flex w-full h-full">
+        <!-- Application information -->
+        <div class="flex flex-col">
+            <!-- Application logo -->
+            <img :src="mla_logo" alt="MLA logo">
+        </div>
     </div>
 </template>
 
 <script>
+import mla_logo from "/public/assets/mla_logo.png";
+
 export default {
-    name: "About"
+    name: "About",
+    data() {
+        return {
+            mla_logo: mla_logo,
+        }
+    }
 }
 </script>
 
