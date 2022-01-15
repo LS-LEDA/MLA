59dce713b9a842e704c71bccd14a78b7cd543c98
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-15 15:14:49 +0000

    #105 refactor: removed contribution badge in general settings page

--- src/pages/Settings/Settings.vue
+++ src/pages/Settings/Settings.vue
@@ -15,29 +15,14 @@
                 </div>
                 <!-- Dashboard deeply nested component view -->
                 <router-view></router-view>
-
-                <!-- Contributors section -->
-                <div class="font-bold text-2xl"> Contributors </div>
-                <div class="flex flex-col sm:grid md:grid-cols-2 lg:grid-cols-3 w-full h-full p-5 gap-5">
-                    <ContributorBadge v-for="(contributor, id) in contributors"
-                                      :key="id"
-                                      :contributor="contributor"
-                    />
-                </div>
             </div>
         </div>
     </section>
 </template>
 
 <script>
-import ContributorBadge from "@/components/UI/ContributorBadge";
-import dani_icon from "/public/assets/dani_amo_profile.jpeg";
-import jiahui_icon from "/public/assets/jiahui_profile.png";
-import default_icon from "/public/assets/default_profile.png";
-
 export default {
     name: "Dashboard",
-    components: {ContributorBadge},
     data() {
         return {
             tabs: [
@@ -52,23 +37,6 @@ export default {
                 {
                     tab_name: "About",
                     tab_path: "/settings/about"
-                }
-            ],
-            contributors: [
-                {
-                    name: "PhD. Daniel Amo Filvà",
-                    email: "daniel.amo@salle.url.edu",
-                    icon: dani_icon,
-                },
-                {
-                    name: "Jiahui Chen",
-                    email: "jiahui1@hotmail.es",
-                    icon: jiahui_icon,
-                },
-                {
-                    name: "Aleix Ollé",
-                    email: "aleix.olle@students.salle.url.edu",
-                    icon: default_icon,
                 }
             ]
         }
