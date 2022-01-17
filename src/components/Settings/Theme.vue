b6d9d5b5d1f5805cf42bf9d88a869fddab431e82
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-17 17:39:27 +0000

    #116 feat: theme component

---
+++ src/components/Settings/Theme.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex flex-col bg-blue-100 rounded-lg w-full h-32 p-5 space-y-2">
+        <!-- Theme name -->
+        <div class="self-center font-bold text-xl">
+            {{ theme.name }}
+        </div>
+
+        <!-- Theme colours -->
+        <div class="flex flex-initial w-full justify-center">
+            <div class="flex justify-center palette">
+                <div class="rounded-full border-2 border-white w-12 h-12 circle bg-[#]"
+                     :class="`bg-[${colour}]`"
+                     v-for="( colour, index ) in theme.colours" :key="index">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Theme",
+    props: ['theme'],
+}
+</script>
+
+<style scoped>
+
+    .palette {
+
+        transform: translateX(20px);
+    }
+
+    .circle{
+        position: relative;
+        left: 0;
+    }
+
+
+    .circle:nth-child(2) {
+        left: -10px;
+    }
+
+    .circle:nth-child(3) {
+        left: -20px;
+    }
+
+    .circle:nth-child(4) {
+        left: -30px;
+    }
+
+    .circle:last-child {
+        left: -40px;
+    }
+
+</style>
