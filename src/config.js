f5a584002f90939fc3d8df2fac94a3ad5e865195
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-19 17:05:45 +0000

    #125 feat: mla settings structure

---
+++ src/config.js
@@ -0,0 +1,46 @@
+const Store = require('electron-store');
+
+const schema = {
+    general: {
+        type: 'object',
+        properties: {
+            gpu: { type: 'boolean' },
+            openOnStartup: { type: 'boolean' },
+            tray: { type: 'boolean' },
+        },
+        default: {
+            gpu: true,
+            openOnStartup: false,
+            tray: true,
+        }
+    },
+    theme: {
+        type: 'object',
+        properties: {
+            modes: {
+                type: 'string',
+                enum: ['dark', 'light', 'system'],
+                default: 'dark',
+            },
+            themes: {
+                type: 'object',
+                properties: {
+                    name: { type: 'string'},
+                    colours: {
+                        type: 'array',
+                        items: {
+                            type: 'string'
+                        },
+                        // TODO: Add default theme
+                        default: [],
+                    }
+                }
+            }
+        }
+    }
+};
+
+// MLA Settings
+const config = new Store({schema});
+
+export default config;
