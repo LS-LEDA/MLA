a09ae728eb2ef087fc6752a1547d1017946a2aa6
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-04 14:59:40 +0000

    #149 feat: interaction popup card title and close button

--- src/components/Summary/InteractionPopUp.vue
+++ src/components/Summary/InteractionPopUp.vue
@@ -3,12 +3,20 @@
          role="dialog" aria-modal="true">
         <!-- PopUp white background -->
         <div class="flex flex-col bg-secondary dark:bg-dark_secondary w-11/12 h-5/6 rounded-xl z-50 p-5 gap-y-5">
-            <!-- Close button -->
-            <button type="button" class=" justify-self-end bg-gray-100 text-gray-400 rounded-lg focus:ring-2 focus:ring-gray-200
-                hover:bg-gray-200 inline-flex w-10 h-10 place-content-center items-center"
-                    @click="close_pop_up">
-                <SvgIcon class="hover:cursor-pointer" type="mdi" :path="close_icon"/>
-            </button>
+            <div class="flex w-full justify-between">
+                <!-- PopUp Title -->
+                <div class="w-auto bg-primary_variant dark:bg-dark_primary_variant rounded-lg py-2 px-5 font-bold">
+                    Total Interactions
+                </div>
+                <!-- Close button -->
+                <button type="button" class="justify-self-end bg-gray-100 text-gray-400 rounded-lg focus:ring-2 focus:ring-gray-200
+                    hover:bg-gray-200 inline-flex w-10 h-10 place-content-center items-center"
+                        @click="close_pop_up">
+                    <SvgIcon class="hover:cursor-pointer" type="mdi" :path="close_icon"/>
+                </button>
+            </div>
+
+
         </div>
         <div class="absolute w-full h-full filter backdrop-blur-sm z-10"></div>
     </div>
