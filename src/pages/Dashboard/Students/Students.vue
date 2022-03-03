ddf6d92ee789b96bcffdea63c4233c1599f6849a
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-03 21:36:14 +0100

    #163 feat: student participation section

--- src/pages/Dashboard/Students/Students.vue
+++ src/pages/Dashboard/Students/Students.vue
@@ -2,11 +2,28 @@
     <h1 class="text-4xl font-extrabold"> Students Data </h1>
     <span class="font-bold text-2xl text-gray-500"> Information on students interactions </span>
     <!-- Student Participation -->
+    <div class="flex w-full h-full mt-5">
+        <div class="flex flex-col w-2/6 h-full bg-secondary dark:bg-dark_secondary rounded-xl p-3 gap-y-3">
+            <span class="flex font-bold text-xl">
+                Student Participation
+            </span>
+            <!-- Student participation list -->
+            <div class="flex flex-col w-full h-full overflow-y-auto">
+                <div class="flex flex-col w-full h-96 gap-y-5 pr-1">
+                    <ParticipationCard v-for="(el, index) in 50" :key="index"/>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import ParticipationCard from "@/components/Students/ParticipationCard";
 export default {
-    name: "Students"
+    name: "Students",
+    components: {
+        ParticipationCard
+    }
 }
 </script>
 
