27c74eb68e598e08ea9f6c5e38476d329d94323e
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-03 14:59:13 +0000

    #160 feat: button component

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="flex h-fit w-fit py-2 px-5 bg-primary hover:bg-primary_variant rounded-lg">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: "Button",
+    props: ['text']
+}
+</script>
+
+<style scoped>
+
+</style>
