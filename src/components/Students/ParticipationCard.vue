fc7da6fd24c29517c10172cfe0dbb889c26e66c0
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-03 21:35:27 +0100

    #163 feat: student participation card component

---
+++ src/components/Students/ParticipationCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex w-full h-fit bg-primary dark:bg-dark_primary rounded-lg p-5">
+        <div class="flex w-8/12">  </div>
+        <div class="flex-none w-2/12">  </div>
+        <div class="flex w-2/12">  </div>
+    </div>
+</template>
+
+<script>
+/**
+ * ParticipationCard is a component for the student
+ * participation table in the students' dashboard page.
+ * It provides information about the amount of tasks
+ * that a student has done in Moodle course
+ * Student Name - Participation % - Amount of tasks
+ */
+export default {
+    name: "ParticipationCard",
+}
+</script>
+
+<style scoped>
+
+</style>
