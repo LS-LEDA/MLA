004f68caad610f4beb961eef0c3af84e0c276f3e
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-03 15:00:19 +0000

    #160 feat: emotions list & import emotions area

--- src/pages/Settings/AI/AI.vue
+++ src/pages/Settings/AI/AI.vue
@@ -1,13 +1,49 @@
 <template>
     <div class="flex w-full h-full gap-x-2">
+        <div class="flex flex-col w-1/3 h-full gap-y-5">
+            <!-- Imported emotions list -->
+            <div class="flex flex-col w-full h-3/4">
+                <div class="flex font-bold text-2xl">
+                    Emotions List
+                </div>
+                <div class="w-full h-full overflow-y-scroll">
+                    <div class="flex flex-col w-full h-96">
+                        <EmotionCard v-for="( emotion, index) in emotions" :key="index" :emotion="emotion"/>
+                    </div>
+                </div>
+            </div>
 
+            <!-- Import emotions box -->
+            <div class="flex flex-col w-full h-1/4 border border-slate-200 rounded-xl p-3 place-items-center gap-y-3">
+                <textarea
+                    class="block w-full h-full text-base font-normal text-gray-700 bg-white bg-clip-padding
+                    border border-solid border-gray-300 rounded transition ease-in-out m-0 border-0
+                    focus:text-gray-700 focus:outline-none bg-transparent resize-none"
+                    id="emotions-input"
+                    placeholder="Enter emotions here"
+                ></textarea>
+                <Button text="Add"/>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
+import EmotionCard from "@/components/Settings/AI/EmotionCard";
+import Button from "@/components/UI/Button";
 
 export default {
     name: "AI",
+    components: {
+        Button,
+        EmotionCard
+    },
+    data() {
+        return {
+            emotions: [],
+        }
+    }
 }
 </script>
 
