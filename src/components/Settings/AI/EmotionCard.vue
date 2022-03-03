402d9cb07d55b46554b2c4dbf8288237c06b771e
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-03 14:16:22 +0000

    #160 feat: emotion card component

---
+++ src/components/Settings/AI/EmotionCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex w-auto h-auto place-items-center">
+        <div class="flex w-full h-fit">
+            {{ emotion }}
+        </div>
+        <IconButton :icon="remove_icon" :type="false" :status="true"/>
+    </div>
+</template>
+
+<script>
+import IconButton from "@/components/UI/IconButton";
+import {mdiClose} from "@mdi/js";
+
+
+/**
+ * EmotionCard is a component for the list of imported
+ * emotions in the AI settings tab.
+ */
+export default {
+    name: "EmotionCard",
+    props: ['emotion'],
+    components: {
+        IconButton
+    },
+    data() {
+        return {
+            remove_icon: mdiClose
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
