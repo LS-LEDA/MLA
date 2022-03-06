bd6e0a901aad116f886af5519f3116370bc6a2ec
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-06 19:37:53 +0100

    #168 refactor: overview & student list layout

--- src/pages/Dashboard/Students/Students.vue
+++ src/pages/Dashboard/Students/Students.vue
@@ -1,55 +1,31 @@
 <template>
-    <h1 class="text-4xl font-extrabold"> Students Data </h1>
-    <span class="font-bold text-2xl text-gray-500"> Information on students interactions </span>
-    <!-- Student Participation -->
-    <div class="flex w-full h-full mt-5 gap-x-5">
-        <div class="flex flex-col w-2/6 h-full bg-secondary dark:bg-dark_secondary rounded-xl p-3 gap-y-3">
-            <div class="flex w-full justify-between">
-                <span class="flex font-bold text-xl">
-                    Student Participation
-                </span>
-                <button type="button" class="items-end place-self-end text-gray-500 rounded-lg hover:bg-gray-300"
-                        @click.stop="this.sp_info = !this.sp_info">
-                    <SvgIcon class="hover:cursor-pointer" type="mdi" :path="sp_info ? close_icon : info_icon"/>
-                </button>
-            </div>
-            <!-- Student participation list -->
-            <div class="flex flex-col w-full h-full overflow-y-auto">
-                <div v-if="!sp_info" class="flex flex-col w-full h-96 gap-y-5 pr-1">
-                    <!-- List header -->
-                    <div class="flex w-full h-auto px-5 font-bold">
-                        <div class="flex w-1/12 justify-center"> ID </div>
-                        <div class="w-8/12"> Student Name </div>
-                        <div class="w-2/12"> # </div>
-                        <div class="w-2/12"> % </div>
-                    </div>
-                    <ParticipationCard v-for="(student, name, index) in students_participation"
-                                       :key="index"
-                                       :id="index"
-                                       :name="name"
-                                       :student="student"
-                    />
-                </div>
-                <!-- Information -->
-                <p v-else>
-                    Total number of interactions between each student and the resources of the course (Includes course viewing).
-                </p>
-            </div>
+    <div class="flex w-full h-auto justify-between">
+        <h1 class="text-4xl font-extrabold"> Students Data </h1>
+        <div class="flex w-auto h-auto gap-x-3">
+            <button class="flex max-w-full max-h-full bg-primary dark:bg-dark_primary rounded-md hover:bg-primary_variant
+            dark:hover:bg-dark_primary_variant aspect-square justify-center items-center"
+                    @click="change_page(-1)"
+            >
+                <SvgIcon type="mdi" :path="left_icon" size="28"/>
+            </button>
+            <button class="flex max-w-full max-h-full bg-primary dark:bg-dark_primary rounded-md hover:bg-primary_variant
+            dark:hover:bg-dark_primary_variant aspect-square justify-center items-center"
+                    @click="change_page(1)"
+            >
+                <SvgIcon type="mdi" :path="right_icon" size="28" @click="change_page(1)"/>
+            </button>
         </div>
-        <StudentDedication/>
     </div>
+    <span class="font-bold text-2xl text-gray-500"> {{ tabs[current_page].tab_description }} </span>
+    <router-view></router-view>
 </template>
 
 <script>
-import ParticipationCard from "@/components/Students/ParticipationCard";
-import {mdiClose, mdiHelpCircleOutline} from "@mdi/js";
+import {mdiMenuLeft, mdiMenuRight} from "@mdi/js";
 import SvgIcon from "@jamescoyle/vue-icon";
-import StudentDedication from "@/components/Students/StudentDedication";
 export default {
     name: "Students",
     components: {
-        StudentDedication,
-        ParticipationCard,
         SvgIcon
     },
     computed: {
