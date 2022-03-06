aeca46db227073e9fd9022436fc042bd1d288711
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-06 13:03:44 +0100

    #166 feat: calculate student dedication estimate

--- src/services/Students/students-processing.js
+++ src/services/Students/students-processing.js
@@ -9,7 +9,9 @@ function student_participation(logs) {
         if ( !(log.id in students) ) {
             students[log.id] = {
                 count: 0,
-                percent: 0.0
+                percent: 0.0,
+                total: 1,
+                timestamp: null,
             }
         }
         students[log.id].count += 1;
@@ -23,4 +25,36 @@ function student_participation(logs) {
     return students;
 }
 
-export { student_participation };
+/**
+ * Loops the logs and for each student
+ * calculate a time dedication estimate
+ * @param logs moodle logs
+ * @param students an object containing all students
+ */
+function student_dedication(logs, students) {
+
+    // Loop from the very end because the logs are in DESC order
+    // which means that the first logs will be the more recent ones
+    // Another option could be sorting the logs array, but that's inefficient
+    for (let i = logs.length - 1; i >= 0; i--) {
+        /*if ( !(logs[i].id in students) ) {
+            students[logs[i].id] = {
+                total: 1,
+                timeField: logs[i].timestamp
+            }
+        }*/
+        // Log timestamp and student's last timestamp difference
+        let min = Math.abs( (logs[i].timestamp - students[logs[i].id].timestamp) / 1000 / 60 );
+
+        if ( min <= 15 ) {
+            // Add minutes difference
+            students[logs[i].id].total += min;
+        } else {
+            // Add 1 minute for each hit below timeSession
+            students[logs[i].id].total++;
+        }
+        students[logs[i].id].timestamp = logs[i].timestamp;
+    }
+}
+
+export { student_participation, student_dedication };
