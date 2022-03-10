ac6d54806d4f1c07e6f0479ae96cb539cba3e6b9
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-10 12:30:44 +0000

    #171 refactor: summary overview turned into a page component

---
+++ src/pages/Dashboard/Summary/SummaryOverview/SummaryOverview.vue
@@ -0,0 +1,115 @@
+<template>
+    <!-- Dashboard Summary Total Interactions card-->
+    <InteractionCard :summary="summary" :logs="logs" @popUp="total_interaction_popup"/>
+    <!-- Dashboard Summary cards-->
+    <div class="grid  gap-x-5 gap-y-5 w-full max-h-full mt-5
+                sm:auto-rows-auto sm:grid-cols-1
+                md:auto-rows-auto md:grid-cols-2
+                xl:auto-rows-auto xl:grid-cols-3">
+        <SummaryCard v-for="(statistic, index) in summary.statistics"
+                     :statistic="statistic" :key="index"
+                     @popUp="detailed_information_pop_up({card_name: statistic.statistic_name, summaryID: index})"/>
+    </div>
+</template>
+
+<script>
+import InteractionCard from "@/components/Summary/InteractionCard";
+import SummaryCard from "@/components/Summary/SummaryCard";
+import {
+    mdiFileDocumentOutline,
+    mdiHammerScrewdriver,
+    mdiLinkVariant,
+    mdiNewspaperVariantOutline,
+    mdiTextBoxCheck, mdiWikipedia
+} from "@mdi/js";
+
+export default {
+    name: "SummaryOverview",
+    components: {
+        InteractionCard,
+        SummaryCard
+    },
+    computed: {
+        summary() {
+            let summary_types = this.$store.state.summary.summary_types;
+            let total_interactions = this.$store.state.summary.total_interactions;
+
+            // TODO: Fix this hardcoded part for future releases. Specially when re-ordering is enabled
+            this.statistics.forEach( (stat, index) => {
+                switch ( stat.statistic_name ) {
+                    case 'Tasks':
+                        if ( !('Tarea' in summary_types) ) return;
+                        stat.number = summary_types['Tarea'].count;
+                        this.$store.commit('saveSummaryCard', {summaryID: index, summary: summary_types['Tarea'].interactions});
+                        break;
+                    case 'URL':
+                        if ( !('URL' in summary_types) ) return;
+                        stat.number = summary_types['URL'].count;
+                        this.$store.commit('saveSummaryCard', {summaryID: index, summary: summary_types['URL'].interactions});
+                        break;
+                }
+            });
+            return { total_interactions: total_interactions, statistics: this.statistics }
+        },
+        logs() {
+            return this.$store.state.logs;
+        },
+    },
+    methods: {
+        detailed_information_pop_up: function ({card_name, summaryID}){
+            this.$root.$emit('popUp', {id: 1, card_name, summaryID});
+        },
+        total_interaction_popup: function () {
+            //this.$emit('popUp', {id: 0})
+            this.$root.$emit('popUp', {id: 0});
+        },
+    },
+    data() {
+        return {
+            statistics:[
+                {
+                    statistic_name: "Tasks",
+                    number: 0,
+                    icon: mdiTextBoxCheck ,
+                    info:"The total number of interactions with all deliveries of a subject."
+                },
+                {
+                    statistic_name: "Files",
+                    number: 0,
+                    icon: mdiFileDocumentOutline,
+                    info:" The total number of interactions with all files of a subject."
+                },
+                {
+                    statistic_name: "Pages",
+                    number: 0,
+                    icon: mdiNewspaperVariantOutline,
+                    info:"The total number of interactions with the pages of a subject."
+                },
+                {
+                    statistic_name: "URL",
+                    number: 0,
+                    icon: mdiLinkVariant,
+                    info: "The total number of interactions with the URL resource of a subject."
+                },
+                {
+                    statistic_name: "Learning Tools Interoperability",
+                    number: 0,
+                    icon: mdiHammerScrewdriver,
+                    info:"The total number of interactions with the learning tools interoperability resources of a subject."
+                },
+                {
+                    statistic_name: "Wiki",
+                    number: 0,
+                    icon: mdiWikipedia ,
+                    info:"The total number of interactions with the wikis of a subject."
+                }
+            ],
+            total_interactions: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
