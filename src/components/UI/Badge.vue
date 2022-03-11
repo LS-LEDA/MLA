e08d3f946f6afafc699b305e8bb968e1e90330d5
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-11 16:39:33 +0000

    #161 feat: badge ui component

---
+++ src/components/UI/Badge.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="text-typography dark:text-dark_typography text-xs font-semibold px-2.5 py-0.5 rounded bg-secondary
+    dark:bg-dark_secondary">
+        {{ text }}
+    </span>
+</template>
+
+<script>
+export default {
+    name: "Badge",
+    props: ['text']
+}
+</script>
+
+<style scoped>
+
+</style>
