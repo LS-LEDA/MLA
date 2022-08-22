126ed6c9696d58ec7cf52374864004ff071913c0
JiahuiChen99 <Jiahui1@hotmail.es>
2022-08-22 17:41:49 +0100

    #180 chore: vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import electron from 'vite-plugin-electron';
+import path from 'path';
+import pkg from './package.json';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        electron({
+            main: {
+                entry: 'electron/main/index.ts',
+                vite: {
+                    build: {
+                        outDir: 'dist/electron/main',
+                    },
+                },
+            },
+            preload: {
+                input: {
+                    // Multiple preload can be configured here
+                    index: path.join(__dirname, 'electron/preload/index.ts'),
+                },
+                vite: {
+                    build: {
+                        // For Debug
+                        sourcemap: false,
+                        outDir: 'dist/electron/preload',
+                    },
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+        // Detect other extensions to SFC imports
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    },
+    server: {
+        host: pkg.env.VITE_DEV_SERVER_HOST,
+        port: pkg.env.VITE_DEV_SERVER_PORT,
+    },
+})
