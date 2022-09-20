6927e205a8d6139b2c70a460acc47a5b0d27f750
JiahuiChen99 <Jiahui1@hotmail.es>
2022-09-20 13:51:49 +0100

    #184 feat: use pinia in app

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from "@/router/router";
+import {createPinia} from "pinia";
 import store from "@/vuex/store";
+
+const pinia = createPinia();
 
 const application = createApp(App)
 application.use(router)
 application.use(store)
+application.use(pinia)
 application.mount('#app')
