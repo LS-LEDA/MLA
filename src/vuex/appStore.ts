4d11eb59ecf8e7db4e2d9618d0d01d1cbc1b02f4
JiahuiChen99 <Jiahui1@hotmail.es>
2022-09-20 17:38:34 +0100

    #184 feat: app pinia store

---
+++ src/vuex/appStore.ts
@@ -0,0 +1,58 @@
+import {defineStore} from "pinia";
+
+interface AppStore {
+    imported_data: object;
+    dashboard: object;
+    forum_file_name: string;
+    forum_messages: [];
+    forum: object;
+}
+
+export const useAppStore = defineStore('app', {
+    state: (): AppStore => ({
+        /**
+         * This value will determine where the navigation
+         * header will redirect to:
+         * If there's uploaded data Dashboard page is
+         * rendered, otherwise Import Data page
+         */
+        // TODO: Change description
+        imported_data: {
+            moodle_logs: false,
+            forum_logs: false,
+        },
+        /**
+         * Course logs related information
+         */
+        dashboard: {
+            logs_file_name: "",
+            course_name: "",
+        },
+        forum_file_name: "",
+        forum_messages: [],
+        forum: {
+            forum_messages: [],
+            messages: 0,
+            users: 0,
+            sentiments: {},
+        },
+    }),
+    actions: {
+        // Call this method if data has been uploaded
+        setImportedData({which, file_name, course_name}) {
+            // Determine the type of imported file: Moodle logs or Forum logs
+            if ( !which ) {
+                this.imported_data.moodle_logs = true;
+                this.dashboard.logs_file_name = file_name;
+                this.dashboard.course_name = course_name;
+            } else {
+                this.imported_data.forum_logs = true;
+                this.forum_file_name = file_name
+            }
+        },
+        // Store the imported forum log
+        storeForumMessages(forum) {
+            this.forum = forum
+        },
+    }
+});
