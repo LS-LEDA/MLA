264f88ebb3b5af969b848997c9af3c5ce29cb30d
JiahuiChen99 <Jiahui1@hotmail.es>
2022-09-24 14:23:54 +0100

    #186 feat: add i18n to download button

--- src/components/NavigationBar/DownloadButton.vue
+++ src/components/NavigationBar/DownloadButton.vue
@@ -2,7 +2,7 @@
     <button type="button" class="flex bg-primary_variant dark:bg-dark_primary_variant rounded-lg px-4 py-2 mx-2 my-2
             block font-bold uppercase justify-center">
         <svg-icon type="mdi" :path="download_icon" v-if="!nav_state"></svg-icon>
-        <span v-if="nav_state">Download</span>
+        <span v-if="nav_state">{{$t("navigation.download")}}</span>
     </button>
 </template>
 
